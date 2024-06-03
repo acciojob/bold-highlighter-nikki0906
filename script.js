0a781b2a1ef61ad0dd8b9ecb249c3dcf802cad08
nikki0906 <160705654+nikki0906@users.noreply.github.com>
2024-06-03 13:39:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 function highlight() {
     //Write your code here
+	const boldWords = document.querySelectorAll('strong');
+    boldWords.forEach(word => {
+        word.style.color = 'green';
+    });
 
 
 }
@@ -7,6 +11,10 @@ function highlight() {
 
 function return_normal() {
     //Write your code here
+	const boldWords = document.querySelectorAll('strong');
+    boldWords.forEach(word => {
+        word.style.color = 'black';
+    });
 
     
 }
